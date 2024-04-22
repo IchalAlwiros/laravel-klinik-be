5c6a75a66b998b05b9b621e0e95d89f893ab010f
IchalAlwiros <ichalwiradev@gmail.com>
2024-04-22 18:58:28 +0700

    add initial code with fortify

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Alwiros Clinic
+  </div>
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.auth.login ');
+});
+
+
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('home', function(){
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+});
